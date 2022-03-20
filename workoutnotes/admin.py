3e092817a3c6a224b06ebe0e755f0123d2819bdb
Kaiyrly <kayrasktl@gmail.com>
2022-03-20 20:54:10 +0900

    added workout and routine to models

--- workoutnotes/admin.py
+++ workoutnotes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from workoutnotes.models import Exercise
+from workoutnotes.models import Exercise, Workout, Routine
 
 admin.site.register(Exercise)
+admin.site.register(Workout)
+admin.site.register(Routine)
 
--- workoutnotes/models.py
+++ workoutnotes/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Workout(models.Model):
+    name = models.TextField(default='My workout')
+    start_time = models.DateTimeField(blank=True, null=True)
+    finish_time = models.DateTimeField(blank=True, null=True)
+    duration = models.DurationField(blank=True, null=True)
+    routine = models.ForeignKey('Routine', null=True, on_delete=models.SET_NULL)
+
+
+    def __str__(self):
+        return self.name
+
 class Exercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=100)
     sets = models.PositiveIntegerField()
     reps = models.PositiveIntegerField()
@@ -13,4 +25,10 @@ class Exercise(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Routine(models.Model):
+    name = models.TextField()
+    exercises = models.JSONField()
 
+    def __str__(self):
+        return self.name
